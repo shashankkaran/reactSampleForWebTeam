ac960e0c71be905bc51c87d7761f854329540ef9
Shashank Karan <shashankkaran999@gmail.com>
2021-03-22 19:09:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+
 import './App.css';
+import cal from './cal.png';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import ThemedDigitalClock from 'themed-digital-clock';
+
+
 
 function App() {
+
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="background" className="aa">
+      <div className="App">
+        <div className="clock">
+
+
+          <img width="60%" id="cal" src={cal} />
+
+          <ThemedDigitalClock
+            description="Mark The Date & Fill the Form "
+            timezoneName="India/New_Delhi"
+            useDarkTheme={true}
+            size={200}
+
+          />
+
+        </div>
+      </div>
+      <div className="form">
+        <form type="submit">
+          <TextField
+            id="a"
+            label="Enter Name"
+            type="name"
+            autoComplete="current-password"
+          />
+          <br />
+          <TextField 
+            id="b"
+            label="Enter Branch"
+            type="name"
+            autoComplete="current-password"
+          />
+          <br />
+          <TextField
+            id="c"
+            label="Enter Year"
+            type="name"
+            autoComplete="current-password"
+          />
+          <br />
+          <TextField
+            id="d"
+            label="Enter Reg Number"
+            type="name"
+            autoComplete="current-password"
+          /><br />
+          <TextField
+            id="e"
+            label="Enter Qualifications"
+            type="name"
+            autoComplete="current-password"
+          />
+        <br/>
+        <br/>
+        <Button variant="contained"  type="submit" color="primary">
+        Submit
+      </Button>
+
+
+        
+        </form>
+      </div>
+
+
+
     </div>
   );
 }
+
 
 export default App;
